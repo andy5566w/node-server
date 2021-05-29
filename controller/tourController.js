f86cdca1894a40590b7cb3635c654ba87400b768
andy tsai <andy5566w@gmail.com>
2021-05-29 14:19:16 +0800

    feat: 新增一欄位isDelete， 新增 增改查 功能

--- controller/tourController.js
+++ controller/tourController.js
@@ -15,3 +15,71 @@ exports.getAllTours = async (req, res) => {
     })
   }
 }
+
+exports.getTourById = async (req, res) => {
+  const tour = await Tour.findById(req.params.id)
+  try {
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: `😱${err}`,
+    })
+  }
+}
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body)
+    res.status(201).json({
+      status: 'success',
+      data: { newTour },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: `😱${err}`,
+    })
+  }
+}
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: `😱${err}`,
+    })
+  }
+}
+
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    tour.isDelete = true
+    const newTour = await Tour.findByIdAndUpdate(req.params.id, tour, {
+      new: true,
+      runValidators: true,
+    })
+    res.status(200).json({
+      status: 'success',
+      data: { newTour },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: `😱${err}`,
+    })
+  }
+}
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,15 @@ const express = require('express')
 const router = express.Router()
 const tourController = require('../controller/tourController')
 
-router.route('/').get(tourController.getAllTours)
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour)
+
+router
+  .route('/:id')
+  .get(tourController.getTourById)
+  .put(tourController.updateTour)
+  .delete(tourController.deleteTour)
 
 module.exports = router
