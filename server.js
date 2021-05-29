2fbb43a6196b9751ea508e377fe33fed6cb9ee1f
andy tsai <andy5566w@gmail.com>
2021-05-29 11:56:46 +0800

    feat: 建立app當作middleware的分流，server建立mongodb連結

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
+const app = require('./app')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+dotenv.config({ path: './config.env' })
 
-app.listen(process.env.PORT || 3000)
-app.get('/', (req,res)=>{
-    res.send({name:'andy'})
-})
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DB_PW
+).replace('<DBNAME>', process.env.DB_NAME)
+
+mongoose
+  .connect(DB, { useNewUrlParser: true })
+  .then(() => console.log('Mongodb connected successfully'))
+  .catch((err) => console.log(err))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log('listening port is ' + PORT))
