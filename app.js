fc15404238be9d42430487337bb6d8a681d44b67
andy tsai <andy5566w@gmail.com>
2021-05-29 22:46:51 +0800

    keroku: 改回原本白名單

--- app.js
+++ app.js
@@ -9,16 +9,15 @@ const app = express()
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
-// const corsOptions = {
-//   origin: [
-//     'https://lucid-hamilton-04c863.netlify.app',
-//     'https://lucid-hamilton-04c863.netlify.app/',
-//     'http://localhost:3006',
-//   ],
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// }
-app.use(cors())
+const corsOptions = {
+  origin: [
+    'https://lucid-hamilton-04c863.netlify.app',
+    'http://localhost:3006',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
